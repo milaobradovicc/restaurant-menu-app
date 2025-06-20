f9c9476b82ce6c72fd6a5b8d533458fa8823d9ab
Mila Obradović <milao@comtrade.com>
2025-06-20 17:27:41 +0200

    Fix: dodala fajl Menu.js za Netlify build

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Menu from "./pages/Menu";
 import AdminLayout from "./admin/AdminLayout";
 import AdminHome from "./admin/AdminHome";
-import Kategorije from "./admin/Kategorije";
+import Kategorije from "./admin/AdminKategorije";
 import Proizvodi from "./pages/Proizvodi"; // <- korisnička strana
 import AdminProizvodi from "./admin/AdminProizvodi"; // <- admin strana
 
@@ -12,7 +11,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
         <Route path="/kategorija/:id" element={<Proizvodi />} />
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<AdminHome />} />
---
+++ client/src/pages/Menu.js
@@ -0,0 +1,4 @@
+function Menu() {
+  return <div>Menu stranica u izradi...</div>;
+}
+export default Menu;
